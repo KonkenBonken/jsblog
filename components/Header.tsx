8e73af6158f0f78a353283754aae76e383b5123d
KonkenBonken <konrad05pettersson@gmail.com>
2023-08-02 01:30:17 +0200

    add header padding

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import AppBar from '@mui/material/AppBar';
 
 export default function Header() {
   return <AppBar position="fixed">
-    <Typography variant="h2" component="h4" gutterBottom>
+    <Typography variant="h2" component="h4" p=".2em 1ch">
       JS Blog
     </Typography>
   </AppBar>;
