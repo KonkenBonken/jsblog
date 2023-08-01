b4d3cd177222d4c1a6ae635937e5f77fb7c9ba77
KonkenBonken <konrad05pettersson@gmail.com>
2023-08-01 23:56:12 +0200

    factor out override props

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -2,10 +2,22 @@
 
 import SyntaxHighlighter from 'react-syntax-highlighter';
 
-export default function CodeSnippet({ children: { props: { children: code } } }: { children: { props: { children: string } } }) {
+const CodeSnippet: Override = ({ children: { props: { children: code } } }) => {
   return <SyntaxHighlighter
     language="javascript" showLineNumbers
   >
     {code}
   </SyntaxHighlighter >;
+};
+
+export default CodeSnippet;
+
+interface OverrideProp {
+  children: {
+    props: {
+      children: string
+    }
+  }
 }
+
+type Override = (props: OverrideProp) => JSX.Element
