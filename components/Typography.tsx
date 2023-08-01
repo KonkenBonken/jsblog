a5f93757aa64bcac8fd6c7eae38fbee6ea90a887
KonkenBonken <konrad05pettersson@gmail.com>
2023-08-02 01:04:31 +0200

    decrease font size

--- components/Typography.tsx
+++ components/Typography.tsx
@@ -3,16 +3,16 @@ import type { Variant } from '@mui/material/styles/createTypography';
 
 import { OverrideTextComponent } from 'utils/override';
 
-function TypographyOverride(variant: Variant) {
+function TypographyOverride(variant: Variant, component: React.ElementType) {
   return OverrideTextComponent(({ content, id }) =>
-    <Typography variant={variant} id={id} gutterBottom>
+    <Typography variant={variant} id={id} component={component} gutterBottom>
       {content}
     </Typography>
   );
 }
 
 export default {
-  H1: TypographyOverride('h1'),
-  H2: TypographyOverride('h2'),
-  P: TypographyOverride('body1'),
+  H1: TypographyOverride('h3', 'h1'),
+  H2: TypographyOverride('h4', 'h2'),
+  P: TypographyOverride('body1', 'p'),
 };
