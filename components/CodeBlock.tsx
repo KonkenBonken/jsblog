5f925f499fdeed0f9902b0789a695d007454f705
KonkenBonken <konrad05pettersson@gmail.com>
2023-08-02 01:14:27 +0200

    display code block inline

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -1,11 +1,17 @@
 "use client";
 
+import type { CSSProperties } from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import OverrideComponent from 'utils/override';
 
 export default OverrideComponent(({ content }) => {
   return <SyntaxHighlighter
-    language="javascript" showLineNumbers
+    language="javascript"
+    showLineNumbers
+    customStyle={{
+      display: 'inline-block',
+      minWidth: '40vw'
+    } satisfies CSSProperties}
   >
     {content}
   </SyntaxHighlighter >;
